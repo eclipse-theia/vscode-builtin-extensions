1d6974d028a48e63dddfc4d72956ee1aa203c2ef
Anton Kosyakov <anton.kosyakov@typefox.io>
2019-03-06 14:47:52 +0000

    upgrade Theia deps
    Signed-off-by: Anton Kosyakov <anton.kosyakov@typefox.io>

--- vscode-builtin-extensions/src/node/vscode-builtin-extensions-backend-module.ts
+++ vscode-builtin-extensions/src/node/vscode-builtin-extensions-backend-module.ts
@@ -1,7 +1,5 @@
 import * as path from 'path';
 import { ContainerModule } from 'inversify';
-import { PluginDeployerDirectoryHandler } from '@theia/plugin-ext';
-import { VSCodeBuiltinExtensionDirectoryHandler } from './vscode-builtin-extension-directory-handler';
 
 let plugins = `local-dir://${path.resolve(__dirname, '../../extensions')}`;
 if (process.env.THEIA_DEFAULT_PLUGINS) {
@@ -9,6 +7,4 @@ if (process.env.THEIA_DEFAULT_PLUGINS) {
 }
 process.env.THEIA_DEFAULT_PLUGINS = plugins;
 
-export default new ContainerModule(bind => {
-    bind(PluginDeployerDirectoryHandler).to(VSCodeBuiltinExtensionDirectoryHandler).inSingletonScope();
-});
+export default new ContainerModule(() => {});
