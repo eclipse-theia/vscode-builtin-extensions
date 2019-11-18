fa978889db1aa40bf808b3190649aadf2373dc23
Anton Kosyakov <anton.kosyakov@typefox.io>
2019-11-18 16:30:58 +0000

    auto publish next version

--- src/publish.js
+++ src/publish.js
@@ -1,14 +1,79 @@
+/**
+ * Republish built-in VS Code extensions to npmjs under `@theia/vscode-builtin-` prefix.
+ */
 // @ts-check
-const rimraf = require('../vscode/node_modules/rimraf');
-const { extensions } = require('./paths.js');
+const fs = require('fs');
+const os = require('os');
+const yargs = require('yargs');
+const archiver = require('archiver');
+const { extensions, run, vscode } = require('./paths.js');
+
+const { tag } = yargs.option('tag', {
+    choices: ['latest', 'next']
+}).demandOption('tag').argv;
+
+// bump to publish
+let version = '0.2.1';
 
 (async () => {
-    await require('./bundle.js');
-    // see why exlcuded https://github.com/theia-ide/theia/issues/3815#issuecomment-452686623
-    rimraf.sync(extensions('configuration-editing'));
-    rimraf.sync(extensions('css-language-features'));
-    rimraf.sync(extensions('html-language-features'));
-    rimraf.sync(extensions('extension-editing'));
-    rimraf.sync(extensions('grunt'));
-    rimraf.sync(extensions('gulp'));
+    if (tag === 'next') {
+        const shortRevision = (await run('git', ['rev-parse', '--short', 'HEAD'], vscode())).trim();
+        const [, minor] = version.split('.');
+        version = `0.${Number(minor) + 1}.0-next.${shortRevision}`;
+    }
+    const result = [];
+    for (const extension of await fs.readdirSync(extensions())) {
+        if (extension.startsWith('ms-vscode')) {
+            // skip marketplace extensions
+            continue;
+        }
+        const pckPath = extensions(extension, 'package.json');
+        if (!fs.existsSync(pckPath)) {
+            continue;
+        }
+        const nodeModulesPath = extensions(extension, 'node_modules');
+        if (fs.existsSync(nodeModulesPath)) {
+            try {
+                await new Promise((resolve, reject) => {
+                    try {
+                        const nodeModulesZip = fs.createWriteStream(extensions(extension, 'vscode_node_modules.zip'));
+                        const archive = archiver('zip');
+                        nodeModulesZip.on('close', () => {
+                            console.log(archive.pointer() + ' total bytes');
+                            console.log('archiver has been finalized and the output file descriptor has closed.');
+                            resolve();
+                        });
+                        archive.on('error', reject);
+                        archive.pipe(nodeModulesZip);
+                        archive.glob('**', { cwd: nodeModulesPath });
+                        archive.finalize();
+                    } catch (e) {
+                        reject(e);
+                    }
+                });
+            } catch (e) {
+                console.error(e);
+                continue;
+            }
+        }
+        const originalContent = fs.readFileSync(pckPath, 'utf-8');
+        const pck = JSON.parse(originalContent);
+        pck.name = '@theia/vscode-builtin-' + pck.name;
+        pck.version = version;
+
+        console.log(pck.name, ': publishing...');
+        try {
+            fs.writeFileSync(pckPath, JSON.stringify(pck, undefined, 2), 'utf-8');
+            await run('yarn', ['publish', '--access', 'public', '--ignore-scripts', '--tag', tag], extensions(extension));
+            result.push(pck.name + ': sucessfully published');
+        } catch (e) {
+            result.push(pck.name + ': failed to publish');
+            if (e) {
+                console.error(e)
+            };
+        } finally {
+            fs.writeFileSync(pckPath, originalContent, 'utf-8');
+        }
+    }
+    console.log(result.join(os.EOL));
 })();
--- src/vscode-bundle.js
+++ src/vscode-bundle.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const rimraf = require('../vscode/node_modules/rimraf');
 const vfs = require('../vscode/node_modules/vinyl-fs');
 const ext = require('../vscode/build/lib/extensions');
-const { root, theiaExtension, extensions, run } = require('./paths.js')
+const { root, theiaExtension, extensions } = require('./paths.js')
 
 const backupNodeModules = () => {
     const move = (src, dest) => fs.existsSync(src) && fs.renameSync(src, dest);
@@ -18,12 +18,9 @@ process.on('SIGINT', revertNodeModules);
 process.on('SIGTERM', revertNodeModules);
 rimraf.sync(extensions());
 (async () => {
-    const stream = ext.packageExtensionsStream().pipe(vfs.dest(theiaExtension()));
+    const stream = ext.packageLocalExtensionsStream().pipe(vfs.dest(theiaExtension()));
     await new Promise((resolve, reject) => {
         stream.on('error', reject);
         stream.on('end', resolve);
     });
-    await run('yarn', ['install', '--production'], extensions('debug-auto-launch'));
-    await run('yarn', ['install', '--production'], extensions('ms-vscode.node-debug'));
-    await run('yarn', ['install', '--production'], extensions('ms-vscode.node-debug2'));
 })();
