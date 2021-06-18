abb5f1bf0574b6378cb68b86606aebd709bcf62e
vince-fugnitto <vincent.fugnitto@ericsson.com>
2021-06-18 12:14:10 -0400

    extension-pack: fix publishing
    The commit updates the `publish-vsix` script to perform the publishing of the builtin extension-pack
    following the publishing of individual builtins. Previously, the publishing of the extension-pack would fail if
    an individual builtin was not yet published to the registry, causing the entire workflow to fail.
    
    Signed-off-by: vince-fugnitto <vincent.fugnitto@ericsson.com>

--- src/publish-vsix.js
+++ src/publish-vsix.js
@@ -16,13 +16,13 @@
 
 /**
  * Publish individual built-in VS Code extensions to an
- * Open VSX registry (default: open-vsx.org) . It is 
+ * Open VSX registry (default: open-vsx.org) . It is
  * assumed that the extensions to be published are present
  * in directory "dist" at the root of this repo.
- * 
+ *
  * The publishing of the extensions is delegated to `ovsx`,
  * which uses the following environment variables to know
- * to which registry to publish-to and what personal 
+ * to which registry to publish-to and what personal
  * authentication token to use to authenticate:
  *  OVSX_REGISTRY_URL, OVSX_PAT
  */
@@ -30,41 +30,64 @@
 const fs = require('fs')
 const os = require('os');
 const ovsx = require('ovsx');
+const { packName } = require('./create-extension-pack.js');
 const { dist } = require('./paths.js');
 const { isPublished } = require('./version');
 
 (async () => {
     const result = [];
 
-    for (const vsix of fs.readdirSync(dist())) {
-        // e.g.: bat-1.45.1.vsix
-        //       css-language-features-1.45.1.vsix
-        //       bat-1.45.2-next.5763d909d5.vsix
-        //       css-language-features-1.45.2-next.5763d909d5.vsix
-        let regexp = /^([\w-]+)-([\d\w\.-]+)\.vsix$/m;
-        const matches = vsix.match(new RegExp(regexp));
-        let [, extension, version] = matches;
-        
-        // is this extension/version alteady published?
-        try {
-            let found = await isPublished(version, extension);
-            if (found) {
-                console.log(`Extension ${extension} v${version} is already published - skipping!`)
-                continue;
-            } 
-        } catch (e) {
-            console.log('error: ' + e)
+    // Publish individual builtin extensions.
+    for (const vsix of fs.readdirSync(dist([]))) {
+        if (vsix.startsWith(packName)) {
+            continue;
         }
-
-        console.log('publishing: ', dist(vsix), ' ...');
-        try {
-            await ovsx.publish({ extensionFile: dist(vsix), yarn: true });
-        } catch (error) {
-            console.log(`failed to publish ${vsix}: ${error}`);
-            console.log('Stopping here. Fix the problem and retry.\n');
-            process.exit(1);
-        }
-        result.push(`Successfully published ${vsix}`);        
+        const publishedVsix = publishExtension(vsix);
+        result.push(`Successfully published: ${publishedVsix}`);
     }
+
+    // Publish builtin extension-pack.
+    for (const vsix of fs.readdirSync(dist([]))) {
+        if (vsix.startsWith(packName)) {
+            const publishedVsix = publishExtension(vsix);
+            result.push(`Successfully published extension-pack: ${publishedVsix}`);
+        }
+    }
+
     console.log(result.join(os.EOL));
 })();
+
+/**
+ * Publish the extension.
+ * @param {*} vsix the vsix extension.
+ */
+async function publishExtension(vsix) {
+
+    // e.g.: bat-1.45.1.vsix
+    //       css-language-features-1.45.1.vsix
+    //       bat-1.45.2-next.5763d909d5.vsix
+    //       css-language-features-1.45.2-next.5763d909d5.vsix
+    let regexp = /^([\w-]+)-([\d\w\.-]+)\.vsix$/m;
+    const matches = vsix.match(new RegExp(regexp));
+    let [, extension, version] = matches;
+
+    // Determine if the extension/version is already published.
+    try {
+        let found = await isPublished(version, extension);
+        if (found) {
+            console.log(`Extension ${extension} v${version} is already published - skipping!`)
+        }
+    } catch (e) {
+        console.log('Error: ' + e)
+    }
+
+    console.log('Publishing: ', dist([vsix]), ' ...');
+    try {
+        await ovsx.publish({ extensionFile: dist([vsix]), yarn: true });
+    } catch (error) {
+        console.log(`Failed to publish ${vsix}: ${error}`);
+        console.log('Stopping here. Fix the problem and retry.\n');
+        process.exit(1);
+    }
+    return vsix;
+}
